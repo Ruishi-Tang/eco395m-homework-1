2d2fe9ae6b745b3834ae8080c30573b38ef6765b
Ruishi Tang <ruishi.tang.0429@gmail.com>
2026-08-28 23:03:04 -0500

    Add make_home.sh

---
+++ home/make_home.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p home
+cd home
+mkdir -p bathroom1 bathroom2 bedroom1 bedroom2 livingroom kitchen
+cd bathroom1
+touch mirror shower sink toilet
+cd ../bathroom2
+touch bathtub mirror shower sink toilet
+cd ../bedroom1 
+touch bed bookshelf chair closet desk lamp
+cd ../bedroom2
+touch air_conditioner bed blinds carpet ceiling_light desk desk_chair floor_lamp wardrobe
+cd ../kitchen
+touch countertop dining_table microwave oven refrigerator sink
+cd ../livingroom
+touch TV carpet floor_lamp sofa table
